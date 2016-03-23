a422a994d574f5a4275f584e0e8b68510704ade6
greglindahl <greglindahl410@gmail.com>
2016-03-23 17:05:57 -0600

    Added some styles to elements page as well as classes to column plugin

--- _plugins/Column.rb
+++ _plugins/Column.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module Column
     def column(text)
-      text.gsub(%r{<ul>}i, '<div class="row">').gsub(%r{</ul>}i, '</div>').gsub(%r{<li>}i, '<div class="col-md-3">').gsub(%r{</li>}i, '</div>')
+      text.gsub(%r{<ul>}i, '<div class="row text-xs-center element-column">').gsub(%r{</ul>}i, '</div>').gsub(%r{<li>}i, '<div class="col-xs-6 col-md-3">').gsub(%r{</li>}i, '</div>')
     end
   end
 end
