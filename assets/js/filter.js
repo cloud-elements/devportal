b6b558e00ac26fe4dbdbc22c099cf56e2173db65
greglindahl <greglindahl410@gmail.com>
2016-04-29 11:25:23 -0600

    Added second case check on filter js file to allow user to filter by capital letter

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $('#txtList').keyup(function () {
-      var value = this.value;
+      var value = this.value.toLowerCase();
       $('#filter div.element-column').each(function () {
           if ($(this).text().toLowerCase().search(value) > -1) {
               $(this).show();
