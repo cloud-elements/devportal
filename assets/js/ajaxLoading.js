d5adbb22e178b986a42c5aba6009c18ba31a3631
Clayton Shaver <clayton_shaver@yahoo.com>
2017-08-04 15:29:19 -0600

    in progress

--- assets/js/ajaxLoading.js
+++ assets/js/ajaxLoading.js
@@ -1,25 +1,25 @@
 jQuery(document).ready(function($) {
-    var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+    const siteUrl = 'http://'+(document.location.hostname||document.location.host);
 
     // Make sure that all clicked links that link to your internal website
     // don't just reload the page but execute a History.pushState call
     $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
         e.preventDefault();
-        History.pushState({}, "", this.pathname);
+        console.log("this: ",this);
+        History.pushState({"urlWithQuery": this.href}, "", this.href);
     });
 
     // Catch all History stateChange events
     History.Adapter.bind(window, 'statechange', function(){
-        var State = History.getState();
-
+        let state = History.getState();
+        console.log('the state: ', state);
         // Load the new state's URL via an Ajax Call
-        $.get(State.url, function(data){
+        $.get(state.url, function(data){
             // Replace the "<title>" tag's content
             document.title = $(data).find("title").text();
 
-            // Replace the content of the main container (.content)
-            // If you're using another div, you should change the selector
-            $('.content').html($(data).find('.content'));
+            // Replace the content of the main container (.contentWrapper)
+            $('.contentWrapper').replaceWith($(data).find('.contentWrapper'));
 
             // // If you're using Google analytics, make sure the pageview is registered!
             // _gaq.push(['_trackPageview', State.url]);
